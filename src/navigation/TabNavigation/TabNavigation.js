1fef303cda778bbf6716f125a5f0d32e4dae347c
Victor Orduno <vict.rcastan.s@gmail.com>
2025-11-12 23:50:00 -0700

    feat(tab-navigation): add custom icons, badge and styling to bottom tab navigator

--- src/navigation/TabNavigation/TabNavigation.js
+++ src/navigation/TabNavigation/TabNavigation.js
@@ -2,12 +2,24 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AuthScreen } from "../../screens/Auth";
 import { scrensName } from "../../utils";
 import { HomeStack, WishlistStack, CartStack, AccountStack } from "../stacks";
+import { View } from "react-native";
+import { Badge } from "react-native-paper";
+import AwesomeIcon from "react-native-vector-icons/FontAwesome";
+import { styles } from "./TabNavigation.styles";
 
 const Tab = createBottomTabNavigator();
 
 export function TabNavigation() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: (routeStatus) => setIcon(route, routeStatus),
+        tabBarActiveTintColor: "#000",
+        tabBarStyle: styles.tabBar,
+        tabBarShowLabel: false,
+        headerShown: false,
+      })}
+    >
       <Tab.Screen
         name={scrensName.home.root}
         component={HomeStack}
@@ -30,4 +42,37 @@ export function TabNavigation() {
       />
     </Tab.Navigator>
   );
+}
+
+function setIcon(route, routeStatus) {
+  const totalProducts = 3;
+  let iconName = "";
+  let color = "#fff";
+
+  if (routeStatus.focused) {
+    color = "#149bd0ff";
+  }
+
+  if (route.name === scrensName.home.root) {
+    iconName = "home";
+  }
+  if (route.name === scrensName.wishlist.root) {
+    iconName = "heart";
+  }
+  if (route.name === scrensName.account.root) {
+    iconName = "user";
+  }
+
+  if (route.name === scrensName.cart.root) {
+    return (
+      <View>
+        <AwesomeIcon name="shopping-cart" color={color} style={styles.icon} />
+        {totalProducts > 0 && (
+          <Badge style={styles.totalCart}>{totalProducts}</Badge>
+        )}
+      </View>
+    );
+  }
+
+  return <AwesomeIcon name={iconName} color={color} style={styles.icon} />;
 }
