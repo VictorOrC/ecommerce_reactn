35fd262dd9598329ab5e9af2bcd64c1e12a3be63
Victor Orduno <vict.rcastan.s@gmail.com>
2025-11-22 21:14:56 -0700

    feat(auth): add global form styles and integrate them into RegisterForm + improved AuthScreen toggle logic

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,16 +1,43 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View } from "react-native";
+import { TextInput, Button } from "react-native-paper";
+import { globalStyles } from "../../../styles";
 
-export function RegisterForm() {
+export function RegisterForm(props) {
+  const { showLogin } = props;
   return (
     <View>
-      <Text>RegisterForm</Text>
-      <Text>RegisterForm</Text>
-      <Text>RegisterForm</Text>
-      <Text>RegisterForm</Text>
-      <Text>RegisterForm</Text>
-      <Text>RegisterForm</Text>
-      <Text>RegisterForm</Text>
+      <TextInput
+        label="Correo electronico"
+        style={globalStyles.form.input}
+        autoCapitalize="none"
+      />
+      <TextInput
+        label="Nombre de usuario"
+        style={globalStyles.form.input}
+        autoCapitalize="none"
+      />
+      <TextInput
+        label="Contraseña"
+        style={globalStyles.form.input}
+        secureTextEntry
+      />
+      <TextInput
+        label="Repetir contraseña"
+        style={globalStyles.form.input}
+        secureTextEntry
+      />
+
+      <Button mode="contained" style={globalStyles.form.btnSubmit}>
+        Registrarte
+      </Button>
+      <Button
+        mode="text"
+        style={globalStyles.form.bntText}
+        labelStyle={globalStyles.form.bntTextLabel}
+        onPress={showLogin}
+      >
+        Iniciar Sesion
+      </Button>
     </View>
   );
 }
--- src/screens/Auth/AuthScreen/AuthScreen.js
+++ src/screens/Auth/AuthScreen/AuthScreen.js
@@ -11,14 +11,21 @@ import logo from "../../../../assets/logo.png";
 import { styles } from "./AuthScreen.styles";
 
 export function AuthScreen() {
-  const [showLogin, setShowLogin] = useState(true);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const onShowLoginRegister = () => setShowLogin((prevState) => !prevState);
+
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
       >
-        {showLogin ? <LoginForm /> : <RegisterForm />}
+        {showLogin ? (
+          <LoginForm />
+        ) : (
+          <RegisterForm showLogin={onShowLoginRegister} />
+        )}
       </KeyboardAvoidingView>
     </View>
   );
